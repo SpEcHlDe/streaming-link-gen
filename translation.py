5356b7fcbe7ec044873802a63a57e244d6d470be
SpEcHlDe <89847967+SpEcHlDe@users.noreply.github.com>
2022-01-31 23:30:54 -0600

    Initial commit

--- translation.py
+++ translation.py
@@ -1,17 +1,21 @@
 class Translation(object):
-    START_TEXT = """Hey [{}](tg://user?id={}), I am Video Streaming Link Generator Bot,
+    START_TEXT = """Hey [{}](tg://user?id={}), I am Streaming Link Generator Bot,
 
 check /help to know more."""
     DOWNLOAD_START = "📥Downloading📥"
     UPLOAD_START = "📤Uploading📤"
-    HELP_USER = """Hey [{}](tg://user?id={}), I am Video Streaming Link Generator Bot,
+    HELP_USER = """Hey [{}](tg://user?id={}), I am Streaming Link Generator Bot,
 
+Just send or forward any file to me and I will send you the direct download
+
+If you send a media file you will get two links, one for download and the other is stream link
+
+   My Commands
 /jw - Stream any JWPlayer video .
 /brightcove - Stream any BrightCove video.
 /yt - Stream any youtube video.
 /play - stream any video with direct link.
 /m3u8 - Stream any m3u8 link.
 /mpd - Stream any mpd link.
-send media - Stream telegram files in Website.
 /status - Check bot usage.
 """
