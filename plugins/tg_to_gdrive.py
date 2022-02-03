18c8b58f04166feb4f47958b37c99ea8addb05a5
SpEcHlDe <89847967+SpEcHlDe@users.noreply.github.com>
2022-02-03 09:37:13 -0600

    Update tg_to_gdrive.py

--- plugins/tg_to_gdrive.py
+++ plugins/tg_to_gdrive.py
@@ -71,21 +71,22 @@ async def tg_to_gdrive_upload(bot, update):
         via = '.mp4', '.mkv', 'avi', '.ts', '.webm', '.flv', '.wmv', '.mov', '.gif', '.mp3', '.m4a', '.ogg', '.wma', 'aac', 'wav' ,'flac'
         forbid = 'xxx', 'sex', 'porn'
         if any(x in up_name.lower() for x in forbid):
-            if update.from_user.id not in Config.AUTH_USERS:
-                GENERATED = "You will be banned soon😈😈"
-            else:
+            if update.from_user.id in Config.AUTH_USERS:
                 GENERATED = f"<b>link Generated</b> \n\n<b>File:</b> [{up_name}]({index_url}) \n\n<b>Size:</b> {size}\n\n<b>Link:</b> [here]({url})"
+            else:
+                GENERATED = "You will be banned soon😈😈"
         elif up_name.lower().endswith(via):
             GENERATED = f"<b>link Generated</b> \n\n<b>File:</b> [{up_name}]({index_url}) \n\n<b>Size:</b> {size}\n\n<b>Link:</b> [here]({url})"
         elif up_name.lower().endswith(image):
-            GENERATED = f"<b>link Generated</b> \n\n<b>File:</b> [{up_name}]({index_url}) \n\n<b>Size:</b> {size}\n\n<b>Link:</b> [here]({url})\n\nI don't think you want to open image on website."
+            GENERATED = f"<b>link Generated</b> \n\n<b>File:</b> [{up_name}]({index_url}) \n\n<b>Size:</b> {size}\n\n<b>Link:</b> I don't think you want to stream image on website."
         else:
             GENERATED = f"<b>Link Generated</b> \n\n<b>File:</b> [{up_name}]({index_url}) \n\n<b>Size:</b> {size}"
-        buttons = [[InlineKeyboardButton(text="Download", url=index_url)]]
+        if update.from_user.id in Config.AUTH_USERS:
+            buttons = [[InlineKeyboardButton(text="Download", url=index_url), InlineKeyboardButton(text="Drive", url=gd_url)]]
+        else:
+            buttons = [[InlineKeyboardButton(text="Download", url=index_url)]]
         if up_name.lower().endswith(video):
             buttons.append([InlineKeyboardButton(text="Stream Video", url=url)])
-        elif up_name.lower().endswith(image):
-            buttons.append([InlineKeyboardButton(text="Open Image On Website", url=url)])
         elif up_name.lower().endswith(audio):
             buttons.append([InlineKeyboardButton(text="Stream Audio", url=url)])
         button_markup = InlineKeyboardMarkup(buttons)
